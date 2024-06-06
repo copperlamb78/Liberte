fe7a717c29a5c7ac8021437744e3696b0dba5124
copperlamb78 <nascimentogabriel.2004@gmail.com>
2024-06-06 11:56:35 -0300

    remove text selector

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,7 +16,7 @@ export const GlobalStyle = createGlobalStyle`
     background-color: ${props => props.theme['gray-800']};
     color: ${props => props.theme['gray-100']};
     -webkit-font-smoothing: antialiased;
-
+    user-select: none;
   }
 
   body, input, textarea, button {
