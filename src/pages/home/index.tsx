9912186d58c22fbe601cbb54da5ed66e472466b2
copperlamb78 <nascimentogabriel.2004@gmail.com>
2024-06-06 01:37:50 -0300

    FirstCommit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../../components/Header";
 import { CartazLiberte, HomeContainer, SocialLinks } from "./styles";
 import cartazImg from "../../assets/cartaz_tiocat.jpg"
-import { DiscordLogo, InstagramLogo, TiktokLogo, YoutubeLogo } from "phosphor-react";
+import { DiscordLogo, InstagramLogo, TiktokLogo} from "phosphor-react";
 
 export function Home() {
     return(
